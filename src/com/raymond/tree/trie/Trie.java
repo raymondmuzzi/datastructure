14e4ef07280cb372583ae8d47766b7afeddab651
raymondmuzzi <raymondmuzzi@163.com>
2020-05-16 22:34:59 +0800

    add new file

--- src/com/raymond/tree/trie/Trie.java
+++ src/com/raymond/tree/trie/Trie.java
@@ -1,9 +1,19 @@
 package com.raymond.tree.trie;
 
+import java.util.TreeMap;
+
 /**
  * @author raymondmuzzi
  * @date 2020-05-13 23:50:33
  */
 public class Trie {
+    private class Node{
+        public boolean isWord;
+        public TreeMap<Character,Node> next;
 
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+    }
 }
